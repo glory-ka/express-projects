ce210401696bdb430e7ceb0185a85875ec54c6d1
G K <ipadglodi@gmail.com>
2022-02-21 19:32:55 +0000

    Added models directory and database files

--- onlineLibrary/app.js
+++ onlineLibrary/app.js
@@ -39,3 +39,12 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = process.DATABASE || '-'; // add a or expression to handle potential error
+
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
