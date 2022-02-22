ac4e0d22c5d323be98b0231278decfa95d9c763e
glory-ka <57584382+glory-ka@users.noreply.github.com>
2022-02-22 01:26:38 +0000

    make code more readable

--- onlineLibrary/app.js
+++ onlineLibrary/app.js
@@ -46,7 +46,8 @@ module.exports = app;
 //Set up mongoose connection
 var mongoose = require('mongoose');
 var mongoDB = process.env.DATABASE || '-'; // add a or expression to handle potential error
-console.log(mongoDB);
+
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
