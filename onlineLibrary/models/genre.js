96b8140e580a27f86e9d437199fc2a1d6196a8aa
G K <ipadglodi@gmail.com>
2022-03-20 01:54:15 -0700

    Add code for genre model

---
+++ onlineLibrary/models/genre.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema (
+    {
+        name: {type:String, required:true, minLength:3, maxLength:100}
+    }
+);
+
+// Create virual property
+GenreSchema
+    .virtual('url')
+    .get(function (){
+        return `/catalog/genre/${this._id}`;
+    });
+
+// Export model
+module.exports = mongoose.model('Genre', GenreSchema);
+
