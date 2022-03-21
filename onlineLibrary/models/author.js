cecfcfb62a38bbbbe380905721c0ab9f2fc7c3e1
G K <ipadglodi@gmail.com>
2022-03-20 22:09:10 -0700

    replace getYear() with getFullYear()

--- onlineLibrary/models/author.js
+++ onlineLibrary/models/author.js
@@ -33,13 +33,13 @@ AuthorSchema.virtual('lifespan').get(function() {
   var lifetime_string = '';
 
   if (this.date_of_birth) {
-    lifetime_string = this.date_of_birth.getYear().toString();
+    lifetime_string = this.date_of_birth.getFullYear().toString();
   }
 
   lifetime_string += ' - ';
 
   if (this.date_of_death) {
-    lifetime_string += this.date_of_death.getYear()
+    lifetime_string += this.date_of_death.getFullYear()
   }
 
   return lifetime_string;
